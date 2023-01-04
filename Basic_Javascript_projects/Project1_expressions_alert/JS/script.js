d38402bd48713e6fa2c0162aebf59fba46c03962
Varstasdp <d.pocius93@gmail.com>
2023-01-04 22:37:46 +0000

    Add my learning with JS

---
+++ Basic_Javascript_projects/Project1_expressions_alert/JS/script.js
@@ -0,0 +1,9 @@
+var X = "Hello boys and girls";
+var Y = "\"Who are you\"" + " He asked me back then.";
+var Mom = "Laima", Dad = "Algis", Sister = "Sima";
+var Mom = Mom.fontcolor("blue");
+var Dad = Dad.fontcolor("blue");
+var Sister = Sister.fontcolor("blue");
+let math = 3+3
+document.write(math);
+window.alert(X);
