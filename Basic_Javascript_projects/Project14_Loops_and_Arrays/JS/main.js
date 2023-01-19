77a3088fb8f4ac9aab3d0b6f308ed74f8a330545
Varstasdp <d.pocius93@gmail.com>
2023-01-19 22:22:10 +0000

    Add For Loop and Array building function to Project14

--- Basic_Javascript_projects/Project14_Loops_and_Arrays/JS/main.js
+++ Basic_Javascript_projects/Project14_Loops_and_Arrays/JS/main.js
@@ -1,3 +1,5 @@
+/* Testing a While loop, this loop will print 
+    Hi there 10 times everytime we press it */
 function callLoop() {
     let i = 0;
     while (i < 10) {
@@ -6,7 +8,30 @@ function callLoop() {
     }
 };
 
+/* This Function taskes user input text and 
+checks the length of that sentence including spaces*/ 
 function checkLength() {
     let length = document.getElementById("sentence").value.length;
     document.getElementById("result2").innerHTML = length;
+};
+
+/* This Function prints a shopping list 
+for a user when he clicks the button */
+function shop() {
+    let products = ["Milk", "Tea", "Eggs", "Bread", "Sausage"];
+    for (let i = 0; i < products.length; i++) {
+        document.getElementById("shoppingList").innerHTML +=  
+        products[i] + "<br>";
+    };
+};
+
+/* This function builds an array and prints one of the elements it to HTLM */
+function arrayFunction() {
+    let colleagues = [];
+    colleagues[0] = "Micheal";
+    colleagues[1] = "Jack";
+    colleagues[2] = "Ionut";
+    colleagues[3] = "Hitesh";
+    document.getElementById("array").innerHTML = 
+    "My favorite workmate is " + colleagues[3];
 };
