abac4886c47129771b10c4067d8104950e8afafb
Varstasdp <d.pocius93@gmail.com>
2023-01-25 22:02:31 +0000

    Add changes to Project 17 include basic JS form validation

--- Basic_Javascript_projects/Project17_JS_Form_Validation/JS/main.js
+++ Basic_Javascript_projects/Project17_JS_Form_Validation/JS/main.js
@@ -1,2 +1,26 @@
-
+function validateForm(){
+    document.getElementById("formAlert").innerHTML = ""
+    let answer;
+    let input = document.forms["myForm"]["clientName"].value;
+    if (input == "") {
+        document.getElementById("formAlert").innerHTML += "Please add you Name*<br>"
+        answer = false;
+    }
+    let input2 = document.forms["myForm"]["phoneNumber"].value;
+    if (input2 == "") {
+        document.getElementById("formAlert").innerHTML += "Please add you Phone Number*<br>"
+        answer = false;
+    }
+    let input3 = document.forms["myForm"]["email"].value;
+    if (input3 == "") {
+        document.getElementById("formAlert").innerHTML += "Please add you Email address*<br>"
+        answer = false;
+    }
+    let input4 = document.forms["myForm"]["message"].value;
+    if (input4 == "") {
+        document.getElementById("formAlert").innerHTML += "Please add Message*<br>"
+        answer = false;
+    }
+    return answer;
+}
 
